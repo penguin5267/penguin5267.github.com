3db10381a6a7cbeb06239eb60af74d1bc14ff7fb
이혜진 <penguin90@sbs.co.kr>
2017-07-21 17:42:30 +0900

    -패럴렉스 스크립트 적용중

--- js/action.js
+++ js/action.js
@@ -1,58 +1,71 @@
-
-//포트폴리오 더보기
+//포트폴리오 목록제어,더보기
 function portfolioList() {
     //목록제어
     var workList = $(".career_list li");
     var workListLength = workList.length;
+    var index = 8;
+    var length  = workListLength - 8;
 
     if (workListLength > 8) {
         for (var workNum = 8; workNum < workListLength; workNum++) {
             workList.eq(workNum).hide();
         }
-        $(".career_inner").addClass("current");
-    } else if (workListLength <= 7) {
-        $(".career_inner").removeClass("current");
     }
 
     //더보기버튼 제어
     $(".btn_list_more").on("click",function(){
-        workList.show();
-        $(".career_inner").addClass("current");
-
-    });
-
-}
-
-    /*
-    //프로그램소개 더보기버튼 리스트 제어
-    function programinfoList() {
-        var programLi = $('.pi_detail_list li');
-        var programLiLenght = programLi.length;
-        //li 갯수체크
-        if (programLiLenght > 7) {
-            for (var ii = 7; ii < programLiLenght; ii++) {
-                programLi.eq(ii).hide();
-            }
-            $('.pibtn_open').addClass('current');
-        } else if (programLiLenght <= 7) {
-            $('.pibtn_open').removeClass('current');
-            //console.log(programLiLenght);
-        }
-        $('.pibtn_open').on("click", function () {
-            $(this).removeClass('current');
-            $(this).parent().children('.pibtn_closed').addClass('current');
-            if (programLiLenght > 7) {
-                programLi.show();
-            }
-        });
-        $('.pibtn_closed').on("click", function () {
-            $(this).removeClass('current');
-            $(this).parent().children('.pibtn_open').addClass('current');
-            if (programLiLenght > 7) {
-                for (var ii = 7; ii < programLiLenght; ii++) {
-                    programLi.eq(ii).hide();
+        if (workListLength > 8) {
+            if(length > 8){
+                length = index +8;
+                for (index; index < length; index++) {
+                    workList.eq(index).show();
+                }
+                length = workListLength -length;
+            }else{
+                length = index + length;
+                for (index; index < length; index++) {
+                    workList.eq(index).show();
                 }
             }
-        });
+        }
+    });
+}
+
+//sns인스타클릭시 안내팝업
+function snsAlert(){
+    $(".snsicon_ig").on("click",function(){
+        alert('Mobile에서만 접속가능합니다');
+    });
+}
+
+//스크롤 모션
+function scrollMotion(){
+    scrollMiddle = $(window).scrollTop()+($(window).height()/2)+300;
+    var $section1 =$("#section1"),
+        $section2 =$("#section2"),
+        $section3 =$("#section3");
+    if(($section1.offset().top<scrollMiddle)&&($section1.offset().top+$section1.height()>scrollMiddle)){
+        console.log("motion1");
+        menuBar(1);
+    }else if ( ($section2.offset().top < scrollMiddle) && ($section2.offset().top + $section2.height() > scrollMiddle) ) {
+        console.log("motion2");
+        menuBar(2);
+    } else if ( ($section3.offset().top < scrollMiddle) && ($section3.offset().top + $section3.height() > scrollMiddle) ) {
+        //$section3.addClass("animate");
+        console.log("motion3");
+        menuBar(3);
     }
-    */
+}
+
+// indicator 표시
+function menuBar(index) {
+    $(".mgl_cont.current").removeClass("current").addClass("current");
+    if (index > 0) {
+        $(".mgl_cont:eq(" + (index - 1) + ")").removeClass("current").addClass("current");
+    }
+}
+
+// section 바로가기
+    function moveSection( val ) {
+        $("html,body").stop().animate({ scrollTop: $("#section" + val).offset().top },400);
+    }
---
+++ js/scroll.js
@@ -0,0 +1,104 @@
+﻿var scrolltopCenter;
+
+$(function(){
+
+	$(window).load(function () {
+		init();
+	});
+
+	$(window).bind('mousewheel',function(){
+		mousewheelEvent();
+	});
+
+	$(window).on('resize', function(){
+		mousewheelEvent();
+	});
+	
+
+	$(window).bind('scroll', function(){
+		scrollMotion();
+	});
+
+});
+
+// 초기화
+function init(){
+	moveSection(1); 
+}
+
+// 마우스 힐 이벤트
+function mousewheelEvent() {
+	if ( $(window).height() > 840 ) { // 최소사이즈 체크 후 제안
+		$('#section1').bind('mousewheel', function(e) {
+			if(e.preventDefault) e.preventDefault();
+			if(e.originalEvent.wheelDelta / 120 > 0) { }
+			else moveSection(2);
+		});
+
+		$('#section2').bind('mousewheel', function(e) {
+			if(e.preventDefault) e.preventDefault();
+			if(e.originalEvent.wheelDelta / 120 > 0) { moveSection(1); }
+			else { moveSection(3); }
+		});
+
+		$('#section3').bind('mousewheel', function(e) {
+			if(e.preventDefault) e.preventDefault();
+			if(e.originalEvent.wheelDelta / 120 > 0) { moveSection(2); }
+			else { moveSection(4); }
+		});
+
+		$('#section4').bind('mousewheel', function(e) {
+			if(e.preventDefault) e.preventDefault();
+			if(e.originalEvent.wheelDelta / 120 > 0) { moveSection(3); }
+			else {  }
+		});
+	} else {
+		$('.section').unbind('mousewheel');
+	}
+}
+
+// 메인 스크롤 모션
+function scrollMotion(){
+	scrolltopCenter = $(window).scrollTop() + ( $(window).height() / 2 ) + 300;
+
+	var $section1 = $("#section1"),
+		$section2 = $("#section2"),
+		$section3 = $("#section3"),
+		$section4 = $("#section4");
+	
+	if ( ($section1.offset().top < scrolltopCenter) && ($section1.offset().top + $section1.height() > scrolltopCenter) ) {		
+		//$section1.addClass("animate");
+		console.log("motion1");
+		showNavi(1);
+	} else if ( ($section2.offset().top < scrolltopCenter) && ($section2.offset().top + $section2.height() > scrolltopCenter) ) {		
+		//$section2.addClass("animate");
+		console.log("motion2");
+		showNavi(2);
+	} else if ( ($section3.offset().top < scrolltopCenter) && ($section3.offset().top + $section3.height() > scrolltopCenter) ) {		
+		//$section3.addClass("animate");
+		console.log("motion3");
+		showNavi(3);
+	} else if ( ($section4.offset().top < scrolltopCenter) && ($section4.offset().top + $section4.height() > scrolltopCenter) ) {
+		//$section4.addClass("animate");
+		console.log("motion4");
+		showNavi(4);
+	}
+}
+
+
+// indicator 표시
+function showNavi(index) {
+	$(".nav li.on").removeClass("on").addClass("off");
+	if ( index > 0) {
+		$(".nav li:eq(" + (index-1) + ")").removeClass("off").addClass("on");
+	}
+
+	$(".nav li .hvr-grow").css({transform:'scale(.6)'});
+	$(".nav li:eq("+ (index-1) +") .hvr-grow").css({transform:'scale(1)'});
+
+}
+
+// section 바로가기
+function moveSection( val ) {
+	$("html,body").stop().animate({ scrollTop: $("#section" + val).offset().top },400);
+}
